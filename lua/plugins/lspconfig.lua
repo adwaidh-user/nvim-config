3be6bab867af73a6f9bd898880ee8404fad162ee
adwaidh-user <adwaith3105@gmail.com>
2025-05-09 00:09:39 +0530

    config for nvim-0.11*

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,56 +1,86 @@
 return {
 	{
-		"williamboman/mason.nvim",
+		"mason-org/mason.nvim",
 		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		lazy = false,
-		opts = {
-			auto_install = true,
-		},
+        dependencies = {
+            "mason-org/mason.nvim",
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+
+            require("mason-lspconfig").setup({
+                automatic_installation = true,
+                handlers = {
+                    -- Default handler for all servers
+                    function (server_name)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities
+                        })
+                    end,
+
+                    -- Custom handlers for servers
+                    ["clangd"] = function ()
+                        lspconfig.clangd.setup({
+                            capabilities = capabilities,
+                            cmd = {
+                                "clangd",
+                                "--background-index",
+                                "--clang-tidy",
+                                "--all-scopes-completion",
+                                "--completion-style=detailed",
+                                "--header-insertion-decorators",
+                                "--header-insertion=iwyu",
+                                "--enable-config",
+                                "--pch-storage=memory",
+                            },
+                            filetypes = { "c", "cpp", "objc", "objcpp" },
+                        })
+                    end,
+                    ["lua_ls"] = function()
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim" }
+                                    }
+                                }
+                            }
+                        })
+                    end
+                },
+            })
+        end,
 	},
 	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.bashls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				cmd = {
-					"clangd",
-					"--background-index",
-					"--clang-tidy",
-					"--all-scopes-completion",
-					"--completion-style=detailed",
-					"--header-insertion-decorators",
-					"--header-insertion=iwyu",
-					"--enable-config",
-					"--pch-storage=memory",
-				},
-			})
-			lspconfig.pyright.setup({
-				capabilities = capabilities,
-			})
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(ev)
+                    local bufnr = ev.buf
+                    local opts = { buffer = bufnr }
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
+                end,
+            })
+        end,
+    },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -14,12 +14,12 @@ return {
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.black,
                 -- linting
-                null_ls.builtins.diagnostics.flake8,
+                null_ls.builtins.diagnostics.pylint,
             },
 
             on_attach = function(client, bufnr)
                 -- Check if clangd is attached to this buffer
-                for _, active_client in ipairs(vim.lsp.get_active_clients({ bufnr = bufnr })) do
+                for _, active_client in ipairs(vim.lsp.get_clients({ bufnr = bufnr })) do
                     if active_client.name == "clangd" then
                         client.offset_encoding = "utf-8"
                         break
