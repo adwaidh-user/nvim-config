56e3047b8b8e66ebd457ae401dbaf5ba0c8a0384
adwaidh-user <adwaith3105@gmail.com>
2025-05-09 00:09:12 +0530

    Added keymap for move/copy line(s) Up/Down

--- lua/myconfig/keymaps.lua
+++ lua/myconfig/keymaps.lua
@@ -5,3 +5,9 @@ local map = vim.keymap.set
 map({'i', 'v'}, '<leader>jk', '<Esc>')
 map({'n', 'v'}, '<leader>q', '<cmd>q<cr>')
 map({'n', 'v'}, '<leader>w', '<cmd>w<cr>')
+-- Copy lines Up and Down
+map({'n', 'x'}, '<M-j>', '<cmd>m.-1<cr>')
+map({'n', 'x'}, '<M-k>', '<cmd>m.1<cr>')
+-- Move lines Up and Down
+map({'n', 'x'}, '<M-J>', '<cmd>co.-1<cr>')
+map({'n', 'x'}, '<M-K>', '<cmd>co.0<cr>')
